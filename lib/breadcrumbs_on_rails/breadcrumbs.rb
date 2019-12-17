579cb020ac6eb63130446615a1613c8590e5e7ec
ck2000 <koch@id-team.com>
2019-12-17 16:19:32 +0100

    Update breadcrumbs.rb

--- lib/breadcrumbs_on_rails/breadcrumbs.rb
+++ lib/breadcrumbs_on_rails/breadcrumbs.rb
@@ -91,14 +91,8 @@ module BreadcrumbsOnRails
           content = @context.link_to_unless_current(compute_name(element), compute_path(element), element.options)
         end
         if @options[:tag]
-          if @context.current_page?
-            @context.content_tag(@options[:tag], class: "breadcrumb-item active") do
-              content
-            end
-          else
-            @context.content_tag(@options[:tag], class: "breadcrumb-item") do
-              content
-            end
+          @context.content_tag(@options[:tag], class: "breadcrumb-item active") do
+            content
           end
         else
           ERB::Util.h(content)
